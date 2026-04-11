3b645baa0e50cf0b711a59b6fed52ef72921ceab
Miro1380 <miro1380@gmail.com>
2026-04-10 20:49:36 -0700

    Added cross origin tag for Product,service controller.

--- demo/src/main/java/com/example/demo/service/OrderService.java
+++ demo/src/main/java/com/example/demo/service/OrderService.java
@@ -12,10 +12,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.example.demo.repository.OrderRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @Slf4j
 @Service
 @RequiredArgsConstructor
--- demo/src/main/java/com/example/demo/service/ProductService.java
+++ demo/src/main/java/com/example/demo/service/ProductService.java
@@ -7,10 +7,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @Service
 @Transactional
 @Slf4j
